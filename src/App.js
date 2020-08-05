47728d15a9db49404e1da58815d1ea0490b1ecc2
mz.zhou <mz.zhou@corp.ybm100.com>
2020-08-05 19:52:11 +0800

    基本路由搭建

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+import Home from './views/Home'
+import Resume from './views/Resume'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         老周的博客
+        <ul className="header">
+          <li>
+            <Link to="/home">home</Link>
+          </li>
+          <li>
+            <Link to="/resume">resume</Link>
+          </li>
+        </ul>
+        {this.props.children}
       </div>
     )
   }
 }
 
-export default App
+class AppRouter extends Component {
+  render() {
+    return (
+      <Router>
+        <App/>
+        <Route path="/" component={App}>
+          <Route path="home" component={Home} />
+          <Route path="resume" component={Resume} />
+        </Route>
+      </Router>
+    )
+  }
+}
+
+export default AppRouter
